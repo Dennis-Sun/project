e28e51403fad03e92a5a7ae41e730df20ecfda84
Dennis-Sun <sunshuo963@gmail.com>
2017-11-10 16:35:04 -0600

    PartB_1 PartB_2 added

--- PartB_1.py
+++ PartB_1.py
@@ -1,17 +1,17 @@
 from __future__ import print_function
 
 import sys
-
+from pyspark.sql import SparkSession
 from pyspark import SparkContext, SparkConf
 
 def parseTweetsThruLocation(tweet):
     """Parses a line of tweet string."""
-    parts = tweet.rsplit('|',3)[::-1]
-    return parts[0], parts[2]
+    parts = tweet.rsplit('|',3)
+    return parts[2], parts[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print >> sys.stderr, "Usage: PartA.py <inputpath>"
+    if len(sys.argv) != 3:
+        print >> sys.stderr, "Usage: PartA_1.py <inputpath> <outputpath>"
         exit(-1)
 
     # Initialize the spark context.
@@ -21,10 +21,16 @@ if __name__ == "__main__":
 		      .set("spark.eventLog.dir", "hdfs://10.254.0.157/eventLog")
 
     sc = SparkContext(conf=conf)
+    spark = SparkSession(sc)
 
-    # Loads in input file.
+    # Loads input file. Partition based on location. The first item returned becomes the key automatically.
     tweets = sc.textFile(sys.argv[1]).map(lambda tweet: parseTweetsThruLocation(tweet))
     #print("\n\nKEYS: ", tweets.keys().collect(), "\n\n")
     #print("\n\nVALUES: ", tweets.values().collect(), "\n\n")
     tweets = tweets.distinct().groupByKey()
+
+    # Write output using DataFrame, using Parquet format (the only option available for pyspark 2.0.0)
+    tweetsDF = tweets.toDF(["country", "text"])
+    #tweetsDF.write.mode('overwrite').partitionBy("country").parquet(sys.argv[2])
+    tweetsDF.write.save(sys.argv[2], "parquet", "overwrite", "country")
     sc.stop()
